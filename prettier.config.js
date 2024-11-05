2f679866277924ab0905560cb3c81c69028e0181
Abinash Panda <abinash@prodios.com>
2024-11-05 12:40:14 +0530

    feat(setup): setup shadcn, prettier and linter

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
